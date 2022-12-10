4d412bfc64fa89a39d36538360e4755d06276f89
ModinaEN <modinaen@gmail.com>
2022-12-10 15:20:25 +0300

    программа для подсчета количества чисел больше 0

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿/*Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3*/
+
+Console.Write($"Введите количество чисел М =");
+int M = int.Parse(Console.ReadLine()!);
+int[]massiv = new int[M];
+int count=0;
+for (int i = 0; i<massiv.Length; i++)
+    {   
+        Console.Write($"{i+1} число равно ");
+        massiv[i] = int.Parse(Console.ReadLine()!); // Задаем числа
+    }
+for (int i = 0; i<massiv.Length; i++)
+{
+    if (massiv[i]>0)
+    count=count+1;
+}
+Console.Write($"Количество чисел больше 0 равно {count}");
