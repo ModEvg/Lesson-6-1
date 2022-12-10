93cc61a11a58e1f799cb098b8bc89d19d5945569
ModinaEN <modinaen@gmail.com>
2022-12-10 16:55:31 +0300

    Нахождение координат точки пересечения двух прямых

--- Program.cs
+++ Program.cs
@@ -1,19 +1,25 @@
-﻿/*Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
-0, 7, 8, -2, -2 -> 2
-1, -7, 567, 89, 223-> 3*/
+﻿/*Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)*/
 
-Console.Write($"Введите количество чисел М =");
-int M = int.Parse(Console.ReadLine()!);
-int[]massiv = new int[M];
-int count=0;
-for (int i = 0; i<massiv.Length; i++)
-    {   
-        Console.Write($"{i+1} число равно ");
-        massiv[i] = int.Parse(Console.ReadLine()!); // Задаем числа
-    }
-for (int i = 0; i<massiv.Length; i++)
+Console.Write($"Введите числа b1, k1, b2, k2 \n");
+
+double b1 = chislo("b1");
+double k1 = chislo("k1");
+double b2 = chislo("b2");
+double k2 = chislo("k2");
+
+Console.Write($"Найдем координаты точки пересечения двух прямых y = {k1}*x+{b1} и y = {k2}*x+{b2} \n");
+
+double x;
+double y;
+
+x = Math.Round((b2-b1)/(k1-k2),2);
+y = Math.Round(k1*x+b1,2);
+Console.Write($"Координаты точки перечения равны ({x},{y})");
+
+double chislo (string vv)
 {
-    if (massiv[i]>0)
-    count=count+1;
+    Console.Write($"{vv}=");
+    double res = double.Parse(Console.ReadLine()!);
+    return res;
 }
-Console.Write($"Количество чисел больше 0 равно {count}");
